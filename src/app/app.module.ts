8df96b19f602c69f48010a4a57372676b16ec53e
Felipe Teles <fliptm95@gmail.com>
2018-10-09 16:06:18 -0300

    Criando componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
-    RodapeComponent
+    RodapeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
